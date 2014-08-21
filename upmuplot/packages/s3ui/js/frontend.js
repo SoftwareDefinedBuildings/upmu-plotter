6ff46bbc9e786c2884abbc83e1e355f05ebdf970
Sam Kumar <samkumar99@gmail.com>
2014-08-21 14:58:21 -0700

    Fixed permalink bug

--- upmuplot/packages/s3ui/js/frontend.js
+++ upmuplot/packages/s3ui/js/frontend.js
@@ -12,7 +12,7 @@ function init_frontend(self) {
     self.idata.changedTimes = undefined;
     self.idata.otherChange = undefined;
     self.idata.automaticAxisUpdate = false; // True if axes will be updated without the need for an "Update Axes" button
-    self.idata.initPermalink = window.location.hostname + (function (port) { if (port === '') { return ''; } return ':' + port; })(window.location.port) + window.location.pathname + '?'; // The start of the permalink
+    self.idata.initPermalink = window.location.protocol + "//" + window.location.hostname + (function (port) { if (port === '') { return ''; } return ':' + port; })(window.location.port) + window.location.pathname + '?'; // The start of the permalink
     self.idata.selectedLegendEntry = undefined; // The currently selected legend entry
     self.idata.chart = self.find("svg.chart");
     self.idata.widthFunction = function () { return window.innerWidth * 0.75; };
